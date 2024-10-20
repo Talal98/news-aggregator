7a06ab74ff8d6c92b52dd3273df785f079676679
Talal98 <35541547+Talal98@users.noreply.github.com>
2024-10-21 03:04:40 +0500

    Did some error handling

--- src/services/service.ts
+++ src/services/service.ts
@@ -6,168 +6,183 @@ export const fetchNewsApiArticles = async (
 	category: any,
 	sources: any,
 ) => {
-	let results: any = []
+	try {
+		let results: any = []
 
-	if (category && category.length > 0) {
-		if (category.length > 1) {
-			for (let i = 0; i < category.length; i++) {
+		if (category && category.length > 0) {
+			if (category.length > 1) {
+				for (let i = 0; i < category.length; i++) {
+					const response = await newsApi({
+						q: query,
+						category: category[i],
+					});
+					const responseWithCategory = response.map((article: any) => {
+						return {
+							...article,
+							category: category[i],
+						}
+					})
+					results = results.concat(responseWithCategory)
+				}
+			}
+			else {
 				const response = await newsApi({
 					q: query,
-					category: category[i],
+					category: category[0],
 				});
 				const responseWithCategory = response.map((article: any) => {
 					return {
 						...article,
-						category: category[i],
+						category: category[0],
 					}
 				})
-				results = results.concat(responseWithCategory)
+				results = results.concat(responseWithCategory);
 			}
 		}
-		else {
+
+		if (sources && sources.length > 0) {
+			const sourcesString = sources.join(',');
 			const response = await newsApi({
 				q: query,
-				category: category[0],
+				sources: sourcesString,
 			});
-			const responseWithCategory = response.map((article: any) => {
-				return {
-					...article,
-					category: category[0],
-				}
-			})
-			results = results.concat(responseWithCategory);
+			results = results.concat(response);
 		}
-	}
 
-	if (sources && sources.length > 0) {
-		const sourcesString = sources.join(',');
-		const response = await newsApi({
-			q: query,
-			sources: sourcesString,
-		});
-		results = results.concat(response);
-	}
+		if (results.length === 0 && query) {
+			const response = await newsApi({
+				q: query,
+			});
+			results = response;
+		}
 
-	if (results.length === 0 && query) {
-		const response = await newsApi({
-			q: query,
-		});
-		results = response;
+		return results;
+	} catch (error) {
+		console.error('Error fetching News API articles', error);
+		return [];
 	}
-
-	return results;
 }
 
 export const fetchGuardianArticles = async (
 	query: string,
 	category: any,
 ) => {
-	let results: any = []
+	try {
+		let results: any = []
 
-	if (category && category.length > 0) {
-		if (category.length > 1) {
-			const updatedCategory = category.map((cat: any) => {
-				return categoryToGuardian[cat]
-			});
-			const sectionString = updatedCategory.join('|');
-			const response = await guardianApi({
-				q: query,
-				section: sectionString,
-			});
-			results = response
-		} else {
-			const response = await guardianApi({
-				q: query,
-				section: categoryToGuardian[category[0]],
-			});
-			results = response
+		if (category && category.length > 0) {
+			if (category.length > 1) {
+				const updatedCategory = category.map((cat: any) => {
+					return categoryToGuardian[cat]
+				});
+				const sectionString = updatedCategory.join('|');
+				const response = await guardianApi({
+					q: query,
+					section: sectionString,
+				});
+				results = response
+			} else {
+				const response = await guardianApi({
+					q: query,
+					section: categoryToGuardian[category[0]],
+				});
+				results = response
+			}
 		}
-	}
 
-	if (results.length === 0) {
-		const response = await guardianApi({
-			q: query,
+		if (results.length === 0) {
+			const response = await guardianApi({
+				q: query,
+			});
+			results = response;
+		}
+
+		const mappedResults = results.map((article: any) => {
+			return {
+				title: article?.webTitle,
+				description: article?.fields?.trailText,
+				urlToImage: article?.fields?.thumbnail,
+				publishedAt: article?.webPublicationDate,
+				url: article?.webUrl,
+				source: {
+					name: 'Guardian',
+				},
+				category: guardianToCategory[article.sectionId],
+			}
 		});
-		results = response;
+
+		return mappedResults;
+	} catch (error) {
+		console.error('Error fetching Guardian articles', error);
+		return [];
 	}
-
-	const mappedResults = results.map((article: any) => {
-		return {
-			title: article?.webTitle,
-			description: article?.fields?.trailText,
-			urlToImage: article?.fields?.thumbnail,
-			publishedAt: article?.webPublicationDate,
-			url: article?.webUrl,
-			source: {
-				name: 'Guardian',
-			},
-			category: guardianToCategory[article.sectionId],
-		}
-	});
-
-	return mappedResults;
 }
 
 export const fetchNewYorkTimesArticles = async (
 	query: string,
 	category: any,
 ) => {
-	let results: any = []
+	try {
+		let results: any = []
 
-	if (category && category.length > 0) {
-		if (category.length > 1) {
-			const updatedCategory = category.map((cat: any) => {
-				if (cat === 'Entertainment') {
-					return 'culture'
-				} else {
-					return cat.toLowerCase()
+		if (category && category.length > 0) {
+			if (category.length > 1) {
+				const updatedCategory = category.map((cat: any) => {
+					if (cat === 'Entertainment') {
+						return 'culture'
+					} else {
+						return cat.toLowerCase()
+					}
+				});
+				const sectionString = updatedCategory.join('", "');
+				for (let i = 0; i < 2; i++) {
+					const response = await newYorkTimesApi({
+						q: query,
+						fq: `news_desk:("${sectionString}")`,
+						page: i,
+					});
+					results = results.concat(response)
 				}
-			});
-			const sectionString = updatedCategory.join('", "');
+			} else {
+				const updatedCategory = category[0] === 'Entertainment' ? 'culture' : category[0].toLowerCase();
+				for (let i = 0; i < 2; i++) {
+					const response = await newYorkTimesApi({
+						q: query,
+						fq: `news_desk:("${updatedCategory}")`,
+						page: i,
+					});
+					results = results.concat(response)
+				}
+			}
+		}
+
+		if (results.length === 0) {
 			for (let i = 0; i < 2; i++) {
 				const response = await newYorkTimesApi({
 					q: query,
-					fq: `news_desk:("${sectionString}")`,
 					page: i,
 				});
-				results = results.concat(response)
-			}
-		} else {
-			const updatedCategory = category[0] === 'Entertainment' ? 'culture' : category[0].toLowerCase();
-			for (let i = 0; i < 2; i++) {
-				const response = await newYorkTimesApi({
-					q: query,
-					fq: `news_desk:("${updatedCategory}")`,
-					page: i,
-				});
-				results = results.concat(response)
+				results = results.concat(response);
 			}
 		}
+
+		const mappedResults = results.map((article: any) => {
+			return {
+				title: article?.headline?.main,
+				description: article?.abstract,
+				urlToImage: article?.multimedia?.length > 0 ? `https://www.nytimes.com/${article.multimedia[0].url}` : '',
+				publishedAt: article?.pub_date,
+				url: article?.web_url,
+				source: {
+					name: 'The New York Times',
+				},
+				category: article?.news_desk === 'Culture' ? 'Entertainment' : article?.news_desk,
+			}
+		});
+
+		return mappedResults;
+	} catch (error) {
+		console.error('Error fetching New York Times articles', error);
+		return [];
 	}
-
-	if (results.length === 0) {
-		for (let i = 0; i < 2; i++) {
-			const response = await newYorkTimesApi({
-				q: query,
-				page: i,
-			});
-			results = results.concat(response);
-		}
-	}
-
-	const mappedResults = results.map((article: any) => {
-		return {
-			title: article?.headline?.main,
-			description: article?.abstract,
-			urlToImage: article?.multimedia?.length > 0 ? `https://www.nytimes.com/${article.multimedia[0].url}` : '',
-			publishedAt: article?.pub_date,
-			url: article?.web_url,
-			source: {
-				name: 'The New York Times',
-			},
-			category: article?.news_desk === 'Culture' ? 'Entertainment' : article?.news_desk,
-		}
-	});
-
-	return mappedResults;
 }
